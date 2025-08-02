28acee6f6ac8143165edb3f48844d3373dd31545
Fridgffy <dr.dciagun@gmail.com>
2025-08-02 10:37:12 +0800

    1

--- python/autoclick.py
+++ python/autoclick.py
@@ -2,12 +2,13 @@
 import pyautogui
 import time
 import sys
+import datetime
 
-TIME_INTERVAL = 5
+TIME_INTERVAL = 60
 COUNT = 1
 
 def GetPosition():
-	input("老实点，蹲下，双手抱头，把鼠标放在告警的那个红点上别动")
+	input("[Hint]: 老实点，蹲下，双手抱头，把鼠标放在告警的那个红点上别动")
 	position = pyautogui.position()
 	x = position.x
 	y = position.y
@@ -16,24 +17,35 @@ def GetPosition():
 
 def main():
 	global COUNT
-	sys.stdout.write(f"自动点击ITSM平台告警，时间间隔为{TIME_INTERVAL/60}分钟\n")
-	sys.stdout.write(f"启动时间为：{time.asctime(time.localtime())}\n\n")
+	now = datetime.datetime.now()
+	# sys.stdout.write(f"自动点击ITSM平台告警，时间间隔为{TIME_INTERVAL/60}分钟\n")
+	sys.stdout.write(f"[Notice]: 启动时间为：" + now.strftime("%Y-%m-%d %H:%M:%S") + "\n\n")
 	try:
 		position = GetPosition()
 		x = position[0]
 		y = position[1]
-		sys.stdout.write(f"\n鼠标坐标为{x},{y}\n\n")
+		sys.stdout.write(f"\n[Notice]: 鼠标坐标为{x},{y}\n\n")
 
 		while True:
-			sys.stdout.write(f"\r运行了{COUNT}次 ")
+			sys.stdout.write(f"\r[RUNNING]: 运行了{COUNT}次 ")
 			pyautogui.click(x=x, y=y)
 			time.sleep(TIME_INTERVAL)
 			
 			COUNT =  COUNT + 1
 	except Exception as e:
-		sys.stdout.write(f"\n发生错误：{e}\n")
+		print(str(e))
 	except KeyboardInterrupt:
-		sys.stdout.write("\n\n这个人！就是你，把我程序中断了！\n")
+		sys.stdout.write("\n\n谁把我程序给中断了！\n")
 
 if __name__ == '__main__':
+	logo = r'''
+ _____ _          ______ _   _____ _____ ________  ___
+/  ___| |         | ___ (_) |_   _|_   _/  ___|  \/  |
+\ `--.| |__   __ _| |_/ /_    | |   | | \ `--.| .  . |
+ `--. \ '_ \ / _` | ___ \ |   | |   | |  `--. \ |\/| |
+/\__/ / | | | (_| | |_/ / |  _| |_  | | /\__/ / |  | |
+\____/|_| |_|\__,_\____/|_|  \___/  \_/ \____/\_|  |_/自动点击ITSM平台告警，时间间隔为5分钟
+                                                      
+'''
+	print(logo)
 	main()
---
+++ python/monitor.py
@@ -0,0 +1,100 @@
+import tkinter as tk
+from PIL import ImageGrab
+import time
+import numpy as np
+from plyer import notification
+from playsound import playsound
+import pyautogui
+import sys
+import datetime
+# pip install playsound==1.2.2
+# pip install Pillow
+
+
+def get_coordinate():
+    count = 0
+    xy_array = []
+    while True:
+        if count == 0:
+            print('[Hint]: 用5秒钟时间把鼠标移动到开始位置')
+            time.sleep(5)
+
+            # 获取鼠标当前位置的坐标
+            x, y = pyautogui.position()
+            # print(f'当前鼠标位置的坐标是：x={x}, y={y}')
+            xy_array.append(x)
+            xy_array.append(y)
+            count += 1
+        elif count == 1:
+            print('[Hint]: 再用5秒钟时间把鼠标移动到结束位置')
+            time.sleep(5)
+
+            # 获取鼠标当前位置的坐标
+            x, y = pyautogui.position()
+            # print(f'当前鼠标位置的坐标是：x={x}, y={y}')
+            xy_array.append(x)
+            xy_array.append(y)
+            count += 1
+        else:
+            break
+
+    xy = tuple(xy_array)
+    return xy
+
+
+def get_average_color(x, y, x2, y2):
+    bbox = (x, y, x2, y2)
+    screenshot = ImageGrab.grab(bbox)
+    screenshot_np = np.array(screenshot)
+    average_color_np = screenshot_np.mean(axis=(0, 1))
+    average_color = tuple(map(int, average_color_np))
+    return average_color
+
+# 监控函数
+def Monitor(COORDINATE):
+    # 获取波分区域颜色的平均值
+    initial_color = get_average_color(*COORDINATE)
+    # sys.stdout.write(f'Initial color coordinate is : {initial_color}\n')
+
+    print("\nStart Monitor 波分...\n")
+    try:
+        while True:
+            current_color = get_average_color(*COORDINATE)
+            now = datetime.datetime.now()
+            # 如果颜色发生变化
+            if current_color != initial_color:
+                print('[ ' + now.strftime('%Y-%m-%d %H:%M:%S') + ' ]' + f"  波分区域颜色已变化！")
+                # 播放声音，把文件放到C盘根目录
+                playsound(r'C:\\1.mp3')
+                # 系统告警
+                notification.notify(
+                    title = '波分颜色变化',
+                    message = '波分颜色变化',
+                    app_name = '波分颜色变化',
+                    timeout = 5
+                    )
+                # 更新颜色
+                # initial_color = current_color 
+            # 每隔多长时间检查一次
+            time.sleep(10) 
+            
+    except KeyboardInterrupt:
+        print("\n谁把我程序给停了!")
+    except Exception as e:
+        print(str(e))
+
+if __name__ == '__main__':
+    logo = r"""
+ _____ _          ______ _ _            __           
+/  ___| |         | ___ (_) |          / _|          
+\ `--.| |__   __ _| |_/ /_| |__   ___ | |_ ___ _ __  
+ `--. \ '_ \ / _` | ___ \ | '_ \ / _ \|  _/ _ \ '_ \ 
+/\__/ / | | | (_| | |_/ / | |_) | (_) | ||  __/ | | |
+\____/|_| |_|\__,_\____/|_|_.__/ \___/|_| \___|_| |_| 每10秒检查一次时间变化
+                                                                                                                          
+"""
+    print(logo)
+    print("[Notice]: 别忘了把1.mp3放到C盘根目录")
+
+    COORDINATE = get_coordinate()
+    Monitor(COORDINATE)
