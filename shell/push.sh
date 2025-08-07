4d42a628b951c4e81fdfedf7fdf04a38a8532c32
Fridgffy <dr.dciagun@gmail.com>
2025-08-07 10:39:01 +0800

    1

--- shell/push.sh
+++ shell/push.sh
@@ -4,7 +4,7 @@ RED='\033[1;31m'
 YELLO='\033[1;33m'
 END='\033[0m'
 
-echo -e "${RED}Check Status... ${END}"
+echo -e "${RED}Check Status ${END}"
 status_start="$(git status)"
 if echo "$status_start" | grep -q "nothing to commit";then
 	echo -e "${GREEN}[ Success ] This is the latest!${END}"
@@ -12,15 +12,15 @@ else
 	echo -e "${YELLO}[ Notice ] The git repository has changed!${END}"
 	echo "$status_start"
 	sleep 1
-	echo -e "${RED}Start add... ${END}"
+	echo -e "${RED}Start add ${END}"
 	git add .
 	sleep 1
-	echo -e "${RED}Check Status... ${END}"
+	echo -e "${RED}Check Status ${END}"
 	git status
 	sleep 1
-	echo -e "${RED}Start commit with 1... ${END}"
+	echo -e "${RED}Start commit with 1 ${END}"
 	git commit -m '1'
 	sleep 1
-	echo -e "${RED}Start push... ${END}"
+	echo -e "${RED}Start push ${END}"
 	git push -u origin main
 fi
