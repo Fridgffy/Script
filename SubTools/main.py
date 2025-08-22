989f771f28eb0065254ed5378e938016aaf943f9
Fridgffy <dr.dciagun@gmail.com>
2025-08-22 12:46:20 +0800

    11

---
+++ SubTools/main.py
@@ -0,0 +1,231 @@
+import sys
+import os
+import yaml
+from modules import Toolsdata, Runtools, output, log, sendemail
+import argparse
+import time
+import datetime
+
+
+# determine configuration file and configuration value
+def judge_config_pathname(config_pathname):
+	try:
+		# if config file exists
+		if os.path.exists(config_pathname):
+			return True
+		else:
+			log.log('[ Initial Error ] File config.yml does not exist')
+			sys.exit(1)
+	except Exception as e:
+		log.log('[ Main Error ]  judge_config_pathname: ' + str(e))
+		sys.exit(1)
+
+# get configuration -> main
+def main_config_value(config_pathname):
+	try:
+		with open(config_pathname,'r')as config_file:
+				config_dict = yaml.safe_load(config_file)
+
+				config = config_dict.get('main')
+				r_path = config.get('r_path')
+				o_path = config.get('o_path')
+				r_name = config.get('r_name')
+				root_path = config.get('root_path')
+				subdict = config.get('subdict')
+
+				git_api = config_dict.get('git_api')
+				chaos_api = config_dict.get('chaos_api')
+				# determine configuration value
+				if r_path:
+					if o_path:
+						if r_name:
+							if root_path:
+								if subdict:
+									if os.path.exists(subdict):
+										if os.path.exists(root_path):
+											if os.path.exists(r_path) and os.path.exists(o_path):
+												name,extension = os.path.splitext(r_name)
+												if extension == ".csv":
+													return r_path, o_path, r_name, root_path, git_api, chaos_api, subdict
+												else:
+													log.log('[ Config Error ] r_name value wrong, extension must be .csv')
+													sys.exit(1)
+											else:
+												log.log('[ Config Error ] r_path or o_path value wrong')
+												sys.exit(1)
+										else:
+											log.log('[ Config Error ] root_path value wrong')
+											sys.exit(1)
+									else:
+										log.log('[ Config Error ] subdict value wrong')
+										sys.exit(1)
+								else:
+									log.log('[ Config Error ] subdict value in config.yml does not exist')
+									sys.exit(1)
+							else:
+								log.log('[ Config Error ] root_path value in config.yml does not exist')
+								sys.exit(1)
+						else:
+							log.log('[ Config Error ] r_name value in config.yml does not exist')
+							sys.exit(1)
+					else:
+						log.log('[ Config Error ] o_path value in config.yml does not exist')
+						sys.exit(1)
+				else:
+					log.log('[ Config Error ] r_path value in config.yml does not exist')
+					sys.exit(1)
+	except Exception as e:
+		log.log('[ Main Error ]  main_config_value: ' + str(e))
+		sys.exit(1)
+
+# get configuration -> Runtools
+def runtools_config_value(config_pathname):
+	try:
+		with open(config_pathname,'r') as config_file:
+			config_dict = yaml.safe_load(config_file)
+			config = config_dict.get('Runtools')
+			timeout = config.get('timeout')
+
+			# determine configuration value
+			if timeout:
+				return timeout
+			else:
+				log.log('[ Config Error ] "timeout" must be configurd！')
+				sys.exit(1)
+	except Exception as e:
+		log.log('[ Main Error ]  runtools_config_value: ' + str(e))
+		sys.exit(1)
+
+# get configuration -> sendemail
+def sendemail_config_value(config_pathname):
+	try:
+		with open(config_pathname, 'r') as config_file:
+			config_dict = yaml.safe_load(config_file)
+			config = config_dict.get('E-mail')
+			sender = config.get('sender')
+			password = config.get('password')
+			receiver = config.get('receiver')
+			smtp_server = config.get('smtp_server')
+			smtp_port = config.get('smtp_port')
+
+			if sender and password and receiver and smtp_server and smtp_port:
+				return sender, password, receiver, smtp_server, smtp_port
+			else:
+				log.log('[ Config Error ] E-mail configuration values does not exist')
+				sys.exit(1)
+	except Exception as e:
+		log.log('[ Main Error ] sendemail_config_value: ' + str(e))
+		sys.exit(1)
+	
+# call module Toolsdata and deal with result data
+def Runtool(timeout, r_path, o_path, root_path, target, git_api, chaos_api, subdict):
+	try:
+		# if result dir not empty
+		if not os.listdir(r_path):
+			
+			Runtools.main(timeout, r_path, o_path, root_path, target, git_api, chaos_api, subdict)
+		else:
+			log.log('[ Initial Error ] result dir is not empty')
+			sys.exit(1)
+	except Exception as e:
+		log.log('[ Main Error ] Runtool: ' + str(e))
+		sys.exit(1)
+
+# call module Toolsdata and deal with result data
+def Toolsdataprocess(r_path):
+	try:
+		alltools, allfiles = Toolsdata.main(r_path)
+		total = len(alltools)
+		# determine all tools perform
+		if total == 14:
+			# print processed files
+			print('[ Processing files ]')
+			for file in allfiles:
+				print(file)
+		else:
+			tlist = ['ESD', 'assetfinder', 'chaos', 'ctfr', 'dnsmap', 'fierce', 'findomain', 'github_subdomains', 'aquatone', 'ksubdomain', 'knock', 'subdomainbrute', 'subfinder', 'dnsub']
+			elist = []
+			for t in tlist:
+				try:
+					alltools.index(t)
+				except Exception as e:
+					if 'is not in list' in str(e):
+						log.log(f'[Result file Error ] {t} result file not find')
+	except Exception as e:
+		log.log('[ Main Error ] Toolsdataprocess: ' + str(e))
+		sys.exit(1)
+
+def output_file(r_pathname):
+	try:
+		# write in result file
+		output.write_in(r_pathname)
+	except Exception as e:
+		log.log('[ Main Error ] output_file: ' + str(e))
+		sys.exit(1)
+
+def send_email(start, end, spend, sender, password, receiver, smtp_server, smtp_port, attachments):
+	try:
+		sendemail.send(start, end, spend, sender, password, receiver, smtp_server, smtp_port, attachments)
+	except Exception as e:
+		log.log('[ Main Error ] send_email ' + str(e))
+		sys.exit(1)
+
+if __name__ == '__main__':
+	try:
+		# start time
+		start = datetime.datetime.now()
+		start_time = time.time()
+
+		log.log(f'[ Project execution start ]')
+		# get path configuration
+		log.log(f'[ Determine config imformaion is correct ]')
+		config_pathname = './configs/config.yml'
+		judge_config_pathname(config_pathname)
+
+		### output values
+		# r_path: result path; o_path: output path; r_name: result file name; r_pathname: result path + filename; root_path: project absolute path
+		r_path, o_path, r_name, root_path, git_api, chaos_api, subdict = main_config_value(config_pathname)
+		r_pathname = os.path.join(o_path, r_name)
+
+		### Runtools values
+		timeout = runtools_config_value(config_pathname)
+
+		### email values
+		sender, password, receiver, smtp_server, smtp_port = sendemail_config_value(config_pathname)
+		attachments = [r_pathname, os.path.join(o_path, 'oneforall.csv')]
+
+		### GET command line paramter
+		parser = argparse.ArgumentParser()
+		parser.add_argument('-t', '--target', dest='target', type=str, required=True, nargs=1, help='Target domain')
+		args = parser.parse_args()
+		target = args.target[0]
+
+		### call module Runtools.py
+		Runtool(timeout, r_path, o_path, root_path, target, git_api, chaos_api, subdict)
+		
+		### call module Toolsdata.py
+		log.log('[ start Processing result file ]')
+		Toolsdataprocess(r_path)
+
+		### call module output.py
+		log.log(f'[ Output result file: {r_pathname} ]')
+		output_file(r_pathname)
+		
+		# end time
+		end = datetime.datetime.now()
+		end_time = time.time()
+
+		spend = end_time - start_time
+
+		### send email
+		send_email(start, end, spend, sender, password, receiver, smtp_server, smtp_port, attachments)
+		log.log(f'[ Email sending successfully: To {receiver}]')
+
+		log.log('[ Project execution completed ]\n\n')
+	except Exception as e:
+		log.log('[ Main Error ] main: ' + str(e))
+		sys.exit(1)
+
+
+
+
---
+++ SubTools/modules/Runtools.py
@@ -0,0 +1,76 @@
+#! /usr/bin/env python
+
+import os
+import sys
+import time
+import subprocess
+from . import log
+
+
+def perform(timeout, toolname, command):
+		try:		
+			log.log(f'[ StartTool ] Start running {toolname}, timeout is {timeout} minutes.')
+			if 'findsub_script' in toolname:
+				shcommand = command.split(' ')
+				p = subprocess.Popen(shcommand, text=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+			else:
+				p = subprocess.Popen(command, text=True, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+			code = p.wait(timeout*60)
+			if code == 0:
+				log.log(f'[ ToolSuccess ] {toolname} perform success.\n')
+			else:
+				log.log(f"[ ToolError ] {toolname} -> return: {code}, perform error\n")
+			if p.stderr.read():
+				log.log(f"[ ToolError ] {toolname} -> error information: {p.stderr.read()}\n")
+		except Exception as e:
+				log.log(f'[ ToolError ] {toolname} {str(e)}\n')
+
+
+def command_dict(r_path, root_path, o_path, target, git_api, chaos_api, subdict):
+	try:
+		shell_path = os.path.join(root_path, 'shells')
+		# findsub script absolute path
+		findsub_pathname = os.path.join(shell_path, 'findsub')
+
+		output_config = f" >> {o_path}/output 2>&1"
+		# command dict
+		command = {
+		'oneforall': f'python /root/subdomain/OneForAll/oneforall.py --port large --brute --path {o_path}/oneforall.csv --target {target} run {output_config}',
+		'esd': f'esd -d {target} {output_config}',
+		'move_esd_file': 'mv /tmp/esd/.*.esd /root/result',
+		'ctfr': f'python /root/subdomain/ctfr/ctfr.py -o ${r_path}/ctfr -d {target} ${output_config}',
+		'subdomainsbrute': f'python /root/subdomain/subDomainsBrute/subDomainsBrute.py -o {r_path}/subdomainbrute --full {target} {output_config}',
+		'aquatone': f'aquatone-discover -t 10 -s 1 --fallback-nameservers 8.8.8.8,223.6.6.6,180.76.76.76,1.1.1.1 -d {target} {output_config}',
+		'move_aquatone_file': f'mv /root/aquatone/{target}/hosts.json {r_path}',
+		'ksubdomain': f'ksubdomain -full -l 1 -csv -o {r_path}/ksubdomain.csv -d {target} {output_config}',
+		'github-subdomains': f'github-subdomains -t {git_api} -o {r_path}/github_subdomains -d {target} {output_config}',
+		'chaos': f'chaos -silent -key {chaos_api} -o {r_path}/chaos -d {target} {output_config}',
+		'findsub_script': f'{findsub_pathname} -t {target}',
+		'fierce': f'fierce --wide --domain {target} >> {r_path}/fierce',
+		'dnsub_dns': f'/root/subdomain/dnsub/dnsub --dns 8.8.8.8,223.6.6.6,180.76.76.76,1.1.1.1 --depth 1 -o {r_path}/dnsub_dns.csv -d {target} {output_config}',
+		'dnsub_nodns': f'/root/subdomain/dnsub/dnsub --depth 1 -o {r_path}/dnsub_nodns.csv -d {target} {output_config}',
+		# bruteforce
+		'dnsub_brute': f'/root/subdomain/dnsub/dnsub -f {subdict} --depth 1 -o {r_path}/dnsub_brute.csv -d {target} {output_config}',
+		'ksubdomain_brute': f'ksubdomain -l 1 -f {subdict} -csv -filter-wild -o {r_path}/ksubdomain_brute.csv -d {target} {output_config}',
+		'findomain': f'findomain --ua /root/subdomain/findomain/ua -w {subdict} --pscan -u {r_path}/findomain_brute -t {target} {output_config}',
+		'subdomainsbrute': f'python /root/subdomain/subDomainsBrute/subDomainsBrute.py -o {r_path}/subdomainbrute_brute --full -f {subdict} {target} {output_config}'
+		}
+
+		return command
+	except Exception as e:
+		log.log(f'[ Runtools Error ]  command_dict: {str(e)}\n')
+
+
+
+# main
+def main(timeout, r_path, o_path, root_path, target, git_api, chaos_api, subdict):
+		try:
+			c_dict = command_dict(r_path, root_path, o_path, target, git_api, chaos_api, subdict)
+
+			for toolname in c_dict.keys():
+				command = c_dict.get(toolname)
+				perform(timeout, toolname, command)
+		except Exception as e:
+				log.log('[ Runtools Error ]  main: ' + str(e))
+
